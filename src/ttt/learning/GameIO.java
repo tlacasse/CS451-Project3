56633e2aa214a202786b281ca9c2199b1ea11287
Thomas Lacasse <thomas@kukt.us>
2018-03-25 15:40:02 -0400

    fix nn reading

--- src/ttt/learning/GameIO.java
+++ src/ttt/learning/GameIO.java
@@ -19,15 +19,13 @@ public class GameIO {
 
 		for (int i = 0; i < weights.length; i++) {
 			nodes[i] = weights[i].rows();
-			weights[i].display();
 			bufferSize += Integer.BYTES + (weights[i].rows() * weights[i].columns() * Double.BYTES);
 		}
 		nodes[nodes.length - 1] = weights[weights.length - 1].columns();
 		bufferSize += Integer.BYTES;
 
-		// #layers (int), sizeOfEachLayer (int), weights (doubles)
+		// #layers (int), sizeOfEachLayer (ints), weights (doubles)
 		final ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
-		System.out.println(bufferSize);
 		try (FileOutputStream fos = new FileOutputStream(nnFileName(nodes))) {
 			buffer.putInt(nodes.length);
 			for (Matrix m : weights) {
@@ -45,28 +43,26 @@ public class GameIO {
 		}
 	}
 
-	public static NeuralNetwork loadNetwork(int... nodes) throws IOException {
-		if (nodes.length < 3) {
+	public static NeuralNetwork loadNetwork(int... nameNodes) throws IOException {
+		if (nameNodes.length < 3) {
 			throw new IllegalArgumentException("Must have at least 3 layers.");
 		}
 		final Matrix[] weights;
-		try (FileInputStream fis = new FileInputStream(nnFileName(nodes));
+		final int[] nodes;
+		try (FileInputStream fis = new FileInputStream(nnFileName(nameNodes));
 				DataInputStream reader = new DataInputStream(fis)) {
-			// recreate structure just to be sure
+			// recreate structure just to be sure things are correct
 			final int layers = reader.readInt();
-			System.out.println(layers);
 			nodes = new int[layers];
 			for (int i = 0; i < layers; i++) {
 				nodes[i] = reader.readInt();
-				System.out.println(nodes[i]);
 			}
 			weights = new Matrix[layers - 1];
 			for (int k = 0; k < layers - 1; k++) {
-				final double[][] ds = new double[nodes[k]][nodes[k] + 1];
+				final double[][] ds = new double[nodes[k]][nodes[k + 1]];
 				for (int i = 0; i < nodes[k]; i++) {
-					for (int j = 0; j < nodes[k] + 1; j++) {
+					for (int j = 0; j < nodes[k + 1]; j++) {
 						ds[i][j] = reader.readDouble();
-						System.out.println(ds[i][j]);
 					}
 				}
 				weights[k] = new Matrix(ds);
