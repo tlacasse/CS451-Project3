4a8e08a4183e73d44c4b3310cdfbf847c7572eb2
Thomas Lacasse <thomas@kukt.us>
2018-04-01 14:32:59 -0400

    web: basic grid

--- Website/TicTacToe/app/game.controller.js
+++ Website/TicTacToe/app/game.controller.js
@@ -8,7 +8,33 @@
                 , function ($http) {
             var vm = this;
 
-            vm.test = 'hello world';
+            function getArray(n) {
+                var a = [];
+                for (var i = 0; i < n; i++) {
+                    a.push(i);
+                }
+                return a;
+            }
+
+            function getArray2d(dim, val) {
+                var a = [];
+                for(var i = 0; i < dim; i++){
+                    var b = [];
+                    for(var i = 0; i < dim; i++){
+                        b.push(val);
+                    }
+                    a.push(b);
+                }
+            }
+
+            vm.size = 13;
+            vm.array = getArray(vm.size);
+
+            vm.percentString = String(100 / 13) + '%';
+
+            vm.state = getArray2d(vm.size, 0);
+            vm.class = getArray2d(vm.size, "");
+
 
         }]);
     
