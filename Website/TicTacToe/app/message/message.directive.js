504a357f48563dfbb94a6509e9e49657b835ce8f
Thomas Lacasse <thomas@kukt.us>
2018-04-01 14:45:42 -0400

    web: include error message box

---
+++ Website/TicTacToe/app/message/message.directive.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('tttModule')
+
+        .directive('errorMessage', function () {
+            return {
+                restrict: 'E',
+                scope: {
+                    controller: '='
+                },
+                templateUrl: '/views/message/message.html'
+            };
+        })
+
+    ;  
+}());
+
---
+++ Website/TicTacToe/app/messageFormat.filter.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('tttModule')
+
+        .filter('replaceNewLines', function () {
+            return function (value) {
+                if (value == null) {
+                    return "";
+                }
+                return value.toString().replace('\\r\\n','\n');
+            };
+        })
+
+        .filter('objectToArray', function () {
+            return function (value) {
+                var array = [];
+                for (var k in value) {
+                    if (value.hasOwnProperty(k)) {
+                        array.push([k,value[k]]);
+                    }
+                }
+                return array;
+            };
+        })
+
+    ;
+
+}());
